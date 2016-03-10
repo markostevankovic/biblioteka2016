2bdc81967953be192e2b8caadf1d1bd4b5800581
markostevankovic <markostevankovic95@gmail.com>
2016-03-10 15:17:03 +0100

    Dodati atributi u klasu knjiga. Napravljeni geteri i seteri.

--- Biblioteka2016/src/biblioteka/Knjiga.java
+++ Biblioteka2016/src/biblioteka/Knjiga.java
@@ -2,5 +2,72 @@ package biblioteka;
 
 public class Knjiga 
 {
+	private String naslov;
+	private long isbn;
+	private String autori;
+	private String izdavac;
+	private int izdanje;
+	
+	public Knjiga(String naslov, long isbn, String autori, String izdavac, int izdanje) 
+	{
+		super();
+		this.naslov = naslov;
+		this.isbn = isbn;
+		this.autori = autori;
+		this.izdavac = izdavac;
+		this.izdanje = izdanje;
+	}
+
+	public String getNaslov() 
+	{
+		return naslov;
+	}
+
+	public void setNaslov(String naslov)
+	{
+		this.naslov = naslov;
+	}
+
+	public long getIsbn() 
+	{
+		return isbn;
+	}
+
+	public void setIsbn(long isbn) 
+	{
+		this.isbn = isbn;
+	}
+
+	public String getAutori() 
+	{
+		return autori;
+	}
+
+	public void setAutori(String autori) 
+	{
+		this.autori = autori;
+	}
+
+	public String getIzdavac() 
+	{
+		return izdavac;
+	}
+
+	public void setIzdavac(String izdavac) 
+	{
+		this.izdavac = izdavac;
+	}
+
+	public int getIzdanje() 
+	{
+		return izdanje;
+	}
+
+	public void setIzdanje(int izdanje) 
+	{
+		this.izdanje = izdanje;
+	}
+	
+	
 	
 }
